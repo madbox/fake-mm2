0d0ef1aecb1cf6153c35f469e3730c4a0269fbd8
Barablin Mikhail <mbarablin@at-consulting.ru>
2012-04-16 00:53:22 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FakeMm2::Application.config.secret_token = '5c0307f0d7eb3aca55433325f14c21cde86fe2a544326caacf5fdf48a12b1fb0831cdf70650e88773c439449725c946cf68b9fd356d6b61c8fc6754754f96ab3'
